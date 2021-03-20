7bd9b7234af1ccb34b0edb637bbb8dd1e8eea411
Ocloo Priscilla <ocloopriscy98@gmail.com>
2021-03-20 00:24:48 +0000

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,41 @@
+function Calculator (){
+    return(
+        
+	<div className="container">
+    <div className="row">
+        <div className="col-md-12 col12">
+            <h1 className="text-right">0</h1></div>
+    </div>
+    <div className="row">
+        <div className="col-md-3 col3"><h1>AC</h1></div>
+        <div className="col-md-3 col3"><h1>+/-</h1></div>
+        <div className="col-md-3 col3"><h1>%</h1></div>
+        <div className="col-md-3 col4"><h1>÷</h1></div>
+    </div>
+    <div className="row">
+        <div className="col-md-3 col3"><h1>7</h1></div>
+        <div className="col-md-3 col3"><h1>8</h1></div>
+        <div className="col-md-3 col3"><h1>9</h1></div>
+        <div className="col-md-3 col4"><h1>×</h1></div>
+    </div>
+    <div className="row">
+        <div className="col-md-3 col3"><h1>4</h1></div>
+        <div className="col-md-3 col3"><h1>5</h1></div>
+        <div className="col-md-3 col3"><h1>6</h1></div>
+        <div className="col-md-3 col4"><h1>-</h1></div>
+    </div>
+    <div className="row">
+        <div className="col-md-3 col3"><h1>1</h1></div>
+        <div className="col-md-3 col3"><h1>2</h1></div>
+        <div className="col-md-3 col3"><h1>3</h1></div>
+        <div className="col-md-3 col4"><h1>+</h1></div>
+    </div>
+    <div className="row">
+        <div className="col-md-6 col3"><h1>0</h1></div>
+        <div className="col-md-3 col3"><h1>.</h1></div>
+        <div className="col-md-3 col4"><h1>=</h1></div>
+    </div>	
+</div>
+    )
+}
+export default Calculator;
